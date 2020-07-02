dd4cbe1a993f50ace7628e97c26a7fa7a35f6195
h1542462994 <t1542462994@outlook.com>
2020-07-02 12:20:28 +0800

    Initial commit

---
+++ DbGrade.Server/Storage/GradeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tro.DbGrade.Server.Storage
+{
+    public class GradeDbContext : DbContext
+    {
+        
+    }
+}
