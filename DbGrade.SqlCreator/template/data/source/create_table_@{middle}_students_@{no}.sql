dee645852ce97d0eda01786c43eaaba9992db0fb
h1542462994 <t1542462994@outlook.com>
2020-07-02 16:03:53 +0800

    添加sqlcreator文件夹示例

---
+++ DbGrade.SqlCreator/template/data/source/create_table_@{middle}_students_@{no}.sql
@@ -0,0 +1,4 @@
+create table @{middle}_Students@{no} (
+    @{short}Sno@{no} char(6) primary key,
+    @{short}Name@{no} char(20) primary key
+)
---
+++ DbGrade.SqlCreator/template/data/target/create_table_yumy_students_1.sql
@@ -0,0 +1,4 @@
+create table yumy_Students1 (
+    ymySno1 char(6) primary key,
+    ymyName1 char(20) primary key
+)
