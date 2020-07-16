e85a29f5d41b0af6ef067c5d3cd44a17f8aa7117
h1542462994 <t1542462994@outlook.com>
2020-07-16 22:35:20 +0800

    创建check和index

--- doc/sql/1_create_tables.sql
+++ doc/sql/1_create_tables.sql
@@ -29,8 +29,8 @@ create table @{middle}_Cities@{no} (
 create table @{middle}_Students@{no} (
     @{short}_Sno@{no} varchar(20) primary key,
     @{short}_Name@{no} varchar(20) not null,
-    @{short}_Sex@{no} int default(0),
-    @{short}_Age@{no} int not null,
+    @{short}_Sex@{no} int default(0) check(@{short}_Sex@{no} in (0,1)),
+    @{short}_Age@{no} int not null check(@{short}_Age@{no} > 0),
     @{short}_TotalCredit@{no} float default(0),
     @{short}_Cno@{no} int not null foreign key references @{middle}_Xclasses@{no} ,
     @{short}_Cino@{no} int not null foreign key references @{middle}_Cities@{no} 
@@ -39,24 +39,24 @@ create table @{middle}_Students@{no} (
 create table @{middle}_Teachers@{no} (
     @{short}_Tno@{no} varchar(20) primary key,
     @{short}_Name@{no} varchar(20) not null,
-    @{short}_Sex@{no} int default(0),
-    @{short}_Age@{no} int default(0),
-    @{short}_Level@{no} int default(0),
+    @{short}_Sex@{no} int default(0) check(@{short}_Sex@{no} in (0,1)),
+    @{short}_Age@{no} int default(0) check(@{short}_Age@{no} > 0),
+    @{short}_Level@{no} int default(0) check(@{short}_Level@{no} in (0,1,2)),
     @{short}_Phone@{no} varchar(11) not null,
 )
 
 create table @{middle}_Terms@{no} (
-    @{short}_Year@{no} int not null,
-    @{short}_Term@{no} int not null,
+    @{short}_Year@{no} int not null check(@{short}_Year@{no} > 0),
+    @{short}_Term@{no} int not null check(@{short}_Term@{no} in (0,1)),
     primary key (@{short}_Year@{no}, @{short}_Term@{no})
 )
 
 create table @{middle}_Courses@{no} (
     @{short}_Cono@{no} int primary key identity,
     @{short}_Name@{no} varchar(20) not null,
-    @{short}_Period@{no} int not null,
-    @{short}_Way@{no} int not null,
-    @{short}_Credit@{no} float default(0)
+    @{short}_Period@{no} int not null check(@{short}_Period@{no} > 0),
+    @{short}_Way@{no} int not null check(@{short}_Way@{no} in (0,1)),
+    @{short}_Credit@{no} float default(0) check (@{short}_Credit@{no} > 0)
 )
 
 create table @{middle}_OpenCourses@{no} (
@@ -73,7 +73,20 @@ create table @{middle}_OpenCourses@{no} (
 create table @{middle}_Reports@{no} (
     @{short}_Ono@{no} int not null foreign key references @{middle}_OpenCourses@{no} ,
     @{short}_Sno@{no} varchar(20) not null foreign key references @{middle}_Students@{no} ,
-    @{short}_Grade@{no} float not null,
+    @{short}_Grade@{no} float not null check (@{short}_Grade@{no} >= 0 and @{short}_Grade@{no} <= 100),
     primary key (@{short}_Ono@{no}, @{short}_Sno@{no})
 )
+
+create index index_OpenCourse_Cno
+on @{middle}_OpenCourses@{no}(@{short}_Cno@{no})
+
+create index index_OpenCourse_Cono
+on @{middle}_OpenCourses@{no}(@{short}_Cono@{no})
+
+create index index_Report_Ono on
+@{middle}_Reports@{no}(@{short}_Ono@{no})
+
+create index index_Report_Sno on
+@{middle}_Reports@{no}(@{short}_Sno@{no})
+
 commit
